4f59127422b4a1be3fae672a6f9dc94d82713696
Jazmin Collins <70544932+jcollins-01@users.noreply.github.com>
2020-10-31 11:46:42 -0600

    Reads all information, overwrites inner stats

--- DND-Sorting-main/DNDSort.java
+++ DND-Sorting-main/DNDSort.java
@@ -2,20 +2,30 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class DNDSort {
+public class DNDSort
+{
+    //One big HashMap that sorts them by chara name, first inner HashMap sorts by their type,
+    //second inner HashMap sorts by the stat and the stat has an associated num value
+    //      *Chara name      *Chara type      *Stat   *Stat level      
+    public static HashMap <String, HashMap <String, HashMap <String, Integer>>> characters = new HashMap<String, HashMap <String, HashMap <String, Integer>>>();
+    //public static HashMap <String, Integer> tempHashInner = new HashMap <String, Integer>();
+    //public static HashMap <String, HashMap<String, Integer>> tempHashOuter = new HashMap <String, HashMap<String, Integer>>();
+    
     public static void main(String[] args) throws FileNotFoundException
+    {   
+        readValues();
+        System.out.println(characters.toString());
+    }
+
+    public static void readValues() throws FileNotFoundException
     {
-        //Currently, when our file reads in data, it doubles the last stat of a character and
-        //assigns it to the character after them. Also, the first character starts with a null
-        //value for stats.
-        
         File file = new File("DND-Sorting-main\\characters.txt"); 
         Scanner scan = new Scanner(file); 
 
         //One big HashMap that sorts them by chara name, first inner HashMap sorts by their type,
         //second inner HashMap sorts by the stat and the stat has an associated num value
         //      *Chara name      *Chara type      *Stat   *Stat level      
-        HashMap <String, HashMap <String, HashMap <String, Integer>>> characters = new HashMap<String, HashMap <String, HashMap <String, Integer>>>();
+        //HashMap <String, HashMap <String, HashMap <String, Integer>>> characters = new HashMap<String, HashMap <String, HashMap <String, Integer>>>();
         
         //Values to place in our HashMap
         String charaName = "";
@@ -25,9 +35,9 @@ public class DNDSort {
         //Used to test while reading our file so we find the right info for each above value
         char testChar;
         char testNextChar;
+        String nullStatString = stat + " (" + statLevel + ")";
+        HashMap <String, Integer> tempHashInner = new HashMap <String, Integer>();
 
-        //We have to read in the information and put it in the put statement effectively
-        //barbarian.put("");
         while (scan.hasNextLine())
         {
             //Reads a whole line at a time so we can operate on each line as we read
@@ -36,31 +46,66 @@ public class DNDSort {
             if(line.length() == 0)
                 continue;
             
-            for(int i = 0; i <line.length(); i++)
+            for(int i = 0; i < line.length(); i++)
             {
                 testChar = line.charAt(i);
-                testNextChar = line.charAt(i + 1);
+                if(i != line.length() - 1)
+                    testNextChar = line.charAt(i + 1);
+                else
+                    testNextChar = '%';
 
                 if(testChar == ' ' && testNextChar == '-')
                     charaType = line.substring(0, i);
-                //If we find our name, we can break out of the loop since it's the last piece we
-                //need from a line with a name in it. Same principle with other break below.
                 else if(testChar == '-' && testNextChar == ' ')
                 {
                     charaName = line.substring(i + 2, line.length());
-                    break;
+                    line = scan.nextLine();
                 }
                 else if(testChar == ':')
                 {
                     stat = line.substring(0, i);
                     statLevel = Integer.parseInt(line.substring(i + 1, line.length()));
+                    tempHashInner.put(stat, statLevel);
                     break;
                 }
-                else
-                    continue;
+                //Checks if our stats come out null or with values, then forces another iteration if null
+                String statString = stat + " (" + statLevel + ")";
+                if(statString.equals(nullStatString))
+                {
+                    for(int j = 0; j < line.length(); j++)
+                    {
+                        char testChar2 = line.charAt(j);
+                        if(testChar2 == ':')
+                        {
+                            stat = line.substring(0, j);
+                            statLevel = Integer.parseInt(line.substring(j + 1, line.length()));
+                            tempHashInner.put(stat, statLevel);
+                            break;
+                        }
+                    }
+                }
             } //End of for-loop
-            System.out.println(charaName + ", " + charaType + " : " + stat + " (" + statLevel + ")");
+            //System.out.println(charaName + ", " + charaType + " : " + stat + " (" + statLevel + ")");
+            //Call a method to place the found values for each chara in our HashMap
+            insertValues(charaName, charaType, tempHashInner);
         } //End of while loop
+    }
+    //      *Chara name      *Chara type      *Stat   *Stat level
+    //HashMap <String, HashMap <String, HashMap <String, Integer>>> characters
+    public static void insertValues(String name, String type, HashMap <String, Integer> tempHashInner)
+    {
+        //HashMap <String, Integer> tempHashInner = new HashMap <String, Integer>();
+        HashMap <String, HashMap<String, Integer>> tempHashOuter = new HashMap <String, HashMap<String, Integer>>();
+        
+        //Copy data from init to temp, clear init, temp will be overwritten each time init comes back with
+        //new info, change argument variable name to initHashInner
+
+        //Some kind of loop that for each specific type value, it adds the new tempHashInner?
+
+
+        //tempHashInner.put(stat, level);
+        tempHashOuter.put(type, tempHashInner);
+        characters.put(name, tempHashOuter);
 
     }
 }
