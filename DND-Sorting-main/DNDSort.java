e7ad09c2ffbabedd8e945b9cb57d8222e3d14438
Jazmin Collins <70544932+jcollins-01@users.noreply.github.com>
2020-11-07 08:10:40 -0700

    Final version of Sorting Program

--- DND-Sorting-main/DNDSort.java
+++ DND-Sorting-main/DNDSort.java
@@ -8,23 +8,26 @@ public class DNDSort
     //second inner HashMap sorts by the stat and the stat has an associated num value
     //      *Chara name      *Chara type      *Stat   *Stat level      
     public static HashMap <String, HashMap <String, HashMap <String, Integer>>> characters = new HashMap<String, HashMap <String, HashMap <String, Integer>>>();
-    
+
     public static void main(String[] args) throws FileNotFoundException
     {   
         readValues();
-        //System.out.println(characters.toString());
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the skill you would like to sort the D&D Characters by:");
+        System.out.println("Welcome to the D&D Character Sorting Program!");
+        System.out.println("\nPlease enter the skill you would like to sort the D&D Characters by:\n");
+        System.out.println("(Enter 'recursion!' to stop the program)");
         String response = scan.nextLine();
 
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 
-        //while(!response.equals("recursion!"))
-        //{
-            addToPQ(response, pq);
-           // pq.clear();
-        //}
-        System.out.println("Thanks for testing it out!");
+        while(!response.equals("recursion!"))
+        {
+            sortValues(response, pq);
+            pq.clear();
+            System.out.println("\nPlease enter the skill you would like to sort the D&D Characters by:\n");
+            response = scan.nextLine();
+        }
+        System.out.println("\nThank you for using the sorting program! We hope you found a character that suits you!");
     }
 
     public static void readValues() throws FileNotFoundException
@@ -194,37 +197,25 @@ public class DNDSort
         characters.put(name, tempHashOuter);
     }//End of insertValues
 
-    public static void addToPQ(String response, PriorityQueue <Integer> pq)
+    public static void sortValues(String response, PriorityQueue <Integer> pq)
     {
         switch(response.toLowerCase())
         {
             case "strength":
             {
                 pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Strength"));
-                //pq.add(barbarian.get("Strength"));
                 pq.add(characters.get("Chip Munk").get("Bard").get("Strength"));
-                //pq.add(bard.get("Strength"));
                 pq.add(characters.get("Barry Cade").get("Cleric").get("Strength"));
-                //pq.add(cleric.get("Strength"));
                 pq.add(characters.get("Brock Lee").get("Druid").get("Strength"));
-                //pq.add(druid.get("Strength"));
                 pq.add(characters.get("Anna Sasin").get("Fighter").get("Strength"));
-                //pq.add(fighter.get("Strength"));
                 pq.add(characters.get("Cam Payne").get("Wizard").get("Strength"));
-                //pq.add(wizard.get("Strength"));
                 pq.add(characters.get("Cara Van").get("Monk").get("Strength"));
-                //pq.add(monk.get("Strength"));
                 pq.add(characters.get("Casey Macy").get("Paladin").get("Strength"));
-                //pq.add(paladin.get("Strength"));
                 pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Strength"));
-                //pq.add(ranger.get("Strength"));
                 pq.add(characters.get("Anne Teak").get("Sorcerer").get("Strength"));
-                //pq.add(sorcerer.get("Strength"));
                 pq.add(characters.get("Anna Prentice").get("Thief").get("Strength"));
-                //pq.add(thief.get("Strength"));
                 pq.add(characters.get("Billy Rubin").get("Warlock").get("Strength"));
-                //pq.add(warlock.get("Strength"));
-
+                System.out.println("\nThe characters are sorted in ascending order by Strength:\n");
                 //put everything here in a while loop for while pq is not empty because we peek
                 //to check the strongest value and if it matches, then we poll
                 while(!pq.isEmpty())
@@ -236,88 +227,170 @@ public class DNDSort
                         //For every Chara type under that name (there's only one for each)
                         for(String type : characters.get(name).keySet())
                         {   
-                            //Get the value at our Strength stat and compare it to the value in pq
+                            //Get the value at our chosen stat and compare it to the value in pq
                             if(comparePQ == characters.get(name).get(type).get("Strength"))
-                                System.out.println(type + " - " + name + "(" + pq.poll() + ")");
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
                         }
                     }
                 }//End of while loop for running through pq
+                break;
             }
-            /*case "dexterity":
+            case "dexterity":
             {
-                pq.add(barbarian.get("Dexterity"));
-                pq.add(bard.get("Dexterity"));
-                pq.add(cleric.get("Dexterity"));
-                pq.add(druid.get("Dexterity"));
-                pq.add(fighter.get("Dexterity"));
-                pq.add(wizard.get("Dexterity"));
-                pq.add(monk.get("Dexterity"));
-                pq.add(paladin.get("Dexterity"));
-                pq.add(ranger.get("Dexterity"));
-                pq.add(sorcerer.get("Dexterity"));
-                pq.add(thief.get("Dexterity"));
-                pq.add(warlock.get("Dexterity"));
+                pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Dexterity"));
+                pq.add(characters.get("Chip Munk").get("Bard").get("Dexterity"));
+                pq.add(characters.get("Barry Cade").get("Cleric").get("Dexterity"));
+                pq.add(characters.get("Brock Lee").get("Druid").get("Dexterity"));
+                pq.add(characters.get("Anna Sasin").get("Fighter").get("Dexterity"));
+                pq.add(characters.get("Cam Payne").get("Wizard").get("Dexterity"));
+                pq.add(characters.get("Cara Van").get("Monk").get("Dexterity"));
+                pq.add(characters.get("Casey Macy").get("Paladin").get("Dexterity"));
+                pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Dexterity"));
+                pq.add(characters.get("Anne Teak").get("Sorcerer").get("Dexterity"));
+                pq.add(characters.get("Anna Prentice").get("Thief").get("Dexterity"));
+                pq.add(characters.get("Billy Rubin").get("Warlock").get("Dexterity"));
+                System.out.println("\nThe characters are sorted in ascending order by Dexterity:\n");
+
+                while(!pq.isEmpty())
+                {
+                    int comparePQ = pq.peek();
+                    for(String name : characters.keySet())
+                    {
+                        for(String type : characters.get(name).keySet())
+                        {   
+                            if(comparePQ == characters.get(name).get(type).get("Dexterity"))
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
+                        }
+                    }
+                }//End of while loop
+                break;
             }
             case "constitution":
             {
-                pq.add(barbarian.get("Consitution"));
-                pq.add(bard.get("Consitution"));
-                pq.add(cleric.get("Consitution"));
-                pq.add(druid.get("Consitution"));
-                pq.add(fighter.get("Consitution"));
-                pq.add(wizard.get("Consitution"));
-                pq.add(monk.get("Consitution"));
-                pq.add(paladin.get("Consitution"));
-                pq.add(ranger.get("Consitution"));
-                pq.add(sorcerer.get("Consitution"));
-                pq.add(thief.get("Consitution"));
-                pq.add(warlock.get("Consitution"));
+                pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Constitution"));
+                pq.add(characters.get("Chip Munk").get("Bard").get("Constitution"));
+                pq.add(characters.get("Barry Cade").get("Cleric").get("Constitution"));
+                pq.add(characters.get("Brock Lee").get("Druid").get("Constitution"));
+                pq.add(characters.get("Anna Sasin").get("Fighter").get("Constitution"));
+                pq.add(characters.get("Cam Payne").get("Wizard").get("Constitution"));
+                pq.add(characters.get("Cara Van").get("Monk").get("Constitution"));
+                pq.add(characters.get("Casey Macy").get("Paladin").get("Constitution"));
+                pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Constitution"));
+                pq.add(characters.get("Anne Teak").get("Sorcerer").get("Constitution"));
+                pq.add(characters.get("Anna Prentice").get("Thief").get("Constitution"));
+                pq.add(characters.get("Billy Rubin").get("Warlock").get("Constitution"));
+                System.out.println("\nThe characters are sorted in ascending order by Constitution:\n");
+
+                while(!pq.isEmpty())
+                {
+                    int comparePQ = pq.peek();
+                    for(String name : characters.keySet())
+                    {
+                        for(String type : characters.get(name).keySet())
+                        {   
+                            if(comparePQ == characters.get(name).get(type).get("Constitution"))
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
+                        }
+                    }
+                }//End of while loop
+                break;
             }
             case "intelligence":
             {
-                pq.add(barbarian.get("Intelligence"));
-                pq.add(bard.get("Intelligence"));
-                pq.add(cleric.get("Intelligence"));
-                pq.add(druid.get("Intelligence"));
-                pq.add(fighter.get("Intelligence"));
-                pq.add(wizard.get("Intelligence"));
-                pq.add(monk.get("Intelligence"));
-                pq.add(paladin.get("Intelligence"));
-                pq.add(ranger.get("Intelligence"));
-                pq.add(sorcerer.get("Intelligence"));
-                pq.add(thief.get("Intelligence"));
-                pq.add(warlock.get("Intelligence"));
+                pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Intelligence"));
+                pq.add(characters.get("Chip Munk").get("Bard").get("Intelligence"));
+                pq.add(characters.get("Barry Cade").get("Cleric").get("Intelligence"));
+                pq.add(characters.get("Brock Lee").get("Druid").get("Intelligence"));
+                pq.add(characters.get("Anna Sasin").get("Fighter").get("Intelligence"));
+                pq.add(characters.get("Cam Payne").get("Wizard").get("Intelligence"));
+                pq.add(characters.get("Cara Van").get("Monk").get("Intelligence"));
+                pq.add(characters.get("Casey Macy").get("Paladin").get("Intelligence"));
+                pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Intelligence"));
+                pq.add(characters.get("Anne Teak").get("Sorcerer").get("Intelligence"));
+                pq.add(characters.get("Anna Prentice").get("Thief").get("Intelligence"));
+                pq.add(characters.get("Billy Rubin").get("Warlock").get("Intelligence"));
+                System.out.println("\nThe characters are sorted in ascending order by Intelligence:\n");
+
+                while(!pq.isEmpty())
+                {
+                    int comparePQ = pq.peek();
+                    for(String name : characters.keySet())
+                    {
+                        for(String type : characters.get(name).keySet())
+                        {   
+                            if(comparePQ == characters.get(name).get(type).get("Intelligence"))
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
+                        }
+                    }
+                }//End of while loop
+                break;
             }
             case "wisdom":
             {
-                pq.add(barbarian.get("Wisdom"));
-                pq.add(bard.get("Wisdom"));
-                pq.add(cleric.get("Wisdom"));
-                pq.add(druid.get("Wisdom"));
-                pq.add(fighter.get("Wisdom"));
-                pq.add(wizard.get("Wisdom"));
-                pq.add(monk.get("Wisdom"));
-                pq.add(paladin.get("Wisdom"));
-                pq.add(ranger.get("Wisdom"));
-                pq.add(sorcerer.get("Wisdom"));
-                pq.add(thief.get("Wisdom"));
-                pq.add(warlock.get("Wisdom"));
+                pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Wisdom"));
+                pq.add(characters.get("Chip Munk").get("Bard").get("Wisdom"));
+                pq.add(characters.get("Barry Cade").get("Cleric").get("Wisdom"));
+                pq.add(characters.get("Brock Lee").get("Druid").get("Wisdom"));
+                pq.add(characters.get("Anna Sasin").get("Fighter").get("Wisdom"));
+                pq.add(characters.get("Cam Payne").get("Wizard").get("Wisdom"));
+                pq.add(characters.get("Cara Van").get("Monk").get("Wisdom"));
+                pq.add(characters.get("Casey Macy").get("Paladin").get("Wisdom"));
+                pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Wisdom"));
+                pq.add(characters.get("Anne Teak").get("Sorcerer").get("Wisdom"));
+                pq.add(characters.get("Anna Prentice").get("Thief").get("Wisdom"));
+                pq.add(characters.get("Billy Rubin").get("Warlock").get("Wisdom"));
+                System.out.println("\nThe characters are sorted in ascending order by Wisdom:\n");
+
+                while(!pq.isEmpty())
+                {
+                    int comparePQ = pq.peek();
+                    for(String name : characters.keySet())
+                    {
+                        for(String type : characters.get(name).keySet())
+                        {   
+                            if(comparePQ == characters.get(name).get(type).get("Wisdom"))
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
+                        }
+                    }
+                }//End of while loop
+                break;
             }
             case "charisma":
             {
-                pq.add(barbarian.get("Charisma"));
-                pq.add(bard.get("Charisma"));
-                pq.add(cleric.get("Charisma"));
-                pq.add(druid.get("Charisma"));
-                pq.add(fighter.get("Charisma"));
-                pq.add(wizard.get("Charisma"));
-                pq.add(monk.get("Charisma"));
-                pq.add(paladin.get("Charisma"));
-                pq.add(ranger.get("Charisma"));
-                pq.add(sorcerer.get("Charisma"));
-                pq.add(thief.get("Charisma"));
-                pq.add(warlock.get("Charisma"));
-            }*/
+                pq.add(characters.get("Alf A. Romeo").get("Barbarian").get("Charisma"));
+                pq.add(characters.get("Chip Munk").get("Bard").get("Charisma"));
+                pq.add(characters.get("Barry Cade").get("Cleric").get("Charisma"));
+                pq.add(characters.get("Brock Lee").get("Druid").get("Charisma"));
+                pq.add(characters.get("Anna Sasin").get("Fighter").get("Charisma"));
+                pq.add(characters.get("Cam Payne").get("Wizard").get("Charisma"));
+                pq.add(characters.get("Cara Van").get("Monk").get("Charisma"));
+                pq.add(characters.get("Casey Macy").get("Paladin").get("Charisma"));
+                pq.add(characters.get("Claire Annette Reed").get("Ranger").get("Charisma"));
+                pq.add(characters.get("Anne Teak").get("Sorcerer").get("Charisma"));
+                pq.add(characters.get("Anna Prentice").get("Thief").get("Charisma"));
+                pq.add(characters.get("Billy Rubin").get("Warlock").get("Charisma"));
+                System.out.println("\nThe characters are sorted in ascending order by Charisma:\n");
+
+                while(!pq.isEmpty())
+                {
+                    int comparePQ = pq.peek();
+                    for(String name : characters.keySet())
+                    {
+                        for(String type : characters.get(name).keySet())
+                        {   
+                            if(comparePQ == characters.get(name).get(type).get("Charisma"))
+                                System.out.println(type + " - " + name + " (" + pq.poll() + ")");
+                        }
+                    }
+                }//End of while loop
+                break;
+            }
+            default:
+            {
+                System.out.println("I'm sorry - that's not a valid statistic for these characters.");
+                System.out.println("Your options are: Strength, Dexterity, Consitution, Intelligence, Wisdom, and Charisma.");
+                System.out.println("And remember, enter 'recursion!' to stop the program.");
+            }
         }//End switch statement
-    }//End addToPQ
+    }//End sortValues
 }
